b85e9358c65a45262486514bf35ac1bb183eb211
josehb96 <josehurtadobaeza@gmail.com>
2025-04-28 02:37:31 +0200

    feat: add RSS feed generation for the blog

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'This is my first blog made with Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
