a99c201d527bbfe992ed1a58492673dbc30482d6
josehb96 <josehurtadobaeza@gmail.com>
2025-04-29 17:07:56 +0200

    chore: update dependencies and enhance TypeScript configuration
    - Added @astrojs/vue and vue as dependencies in package.json
    - Updated TypeScript configuration to include jsx preservation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://my-first-astro-blog-josehb96.netlify.app"
+  site: "https://my-first-astro-blog-josehb96.netlify.app",
+  integrations: [vue()]
 });
