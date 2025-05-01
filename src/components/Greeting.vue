da6e1bddd2f4d211bc21e514cb8619c44df84c5c
josehb96 <josehurtadobaeza@gmail.com>
2025-05-01 02:36:45 +0200

    feat: add Greeting component with random greeting functionality

---
+++ src/components/Greeting.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+  
+  import {ref} from 'vue'
+
+  const {messages} = defineProps<{
+    messages: string[]
+  }>()
+
+  const greeting = ref(messages[0]) 
+
+  const setRandomGreeting = () => {
+    greeting.value = messages[Math.floor(Math.random() * messages.length)];
+  }
+
+</script>
+
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting!</h3>
+    <button @click="setRandomGreeting">New Greeting</button>
+  </div>
+</template>
